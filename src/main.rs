6adb28ae4b59a5b6eba86e40d6552994dd43f5ec
Bram van Meurs <7422223+bcvanmeurs@users.noreply.github.com>
2023-01-20 18:39:24 +0000

    simplify code with `?` and returning Results

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,43 @@
 use std::{fs, path};
 use dirs::home_dir;
+use std::error::Error;
 
-
-fn move_file(homedir: &path::PathBuf, path: &path::PathBuf) {
+fn move_file(homedir: &path::PathBuf, path: path::PathBuf) -> Result<(), Box<dyn Error>>{
     if path.is_file(){
+
         let extension = match path.extension() {
             None => String::from("NOEXT"),
             Some(ext) => ext.to_str().unwrap().to_uppercase(),
         };
-        let new_dir = &homedir
-            .join(&extension);
+        let new_dir = homedir
+            .join(extension);
 
-        fs::create_dir_all(&new_dir).unwrap();
+        fs::create_dir_all(&new_dir)?;
 
         let new_path = &new_dir
-            .join(&path.file_name().unwrap());
+            .join(path.file_name().unwrap());
 
         println!("Move {:?} => {:?}", 
-            &path.strip_prefix(homedir).unwrap().to_str().unwrap(),
-            &new_path.strip_prefix(homedir).unwrap().to_str().unwrap(),
+            path.strip_prefix(homedir)?.to_str().unwrap(),
+            new_path.strip_prefix(homedir)?.to_str().unwrap(),
         );
 
-        fs::rename(&path, &new_path).unwrap();
-
+        fs::rename(path, new_path)?;
     }
+    Ok(())
 }
 
-fn main() {
-    let mut homedir = home_dir().unwrap();
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut homedir = home_dir().ok_or("Error")?;
     homedir.push("Downloads");
     
-    let dirs = fs::read_dir(&homedir).expect("Directory not found!");
+    let dirs = fs::read_dir(&homedir)?;
 
     for dir in dirs {
-        let path = dir.expect("Error reading dir").path();
-
-        move_file(&homedir, &path);
-
+        let path = dir?.path();
+        move_file(&homedir, path).expect("Error moving file");
     }
+    Ok(())
 }
 // Add error handling: unwrap() and expect() should be avoided in production code as they panic on errors. Instead, use match statements or the Result type to handle errors properly.
 // Use as_path() instead of path() when getting the path from a DirEntry
